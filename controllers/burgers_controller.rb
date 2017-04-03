a8bb5793017a270ce3d06e0becf19fcd64d88ea6
James Duncan <james.a.duncan91@gmail.com>
2017-04-03 12:28:17 +0100

    added forms to create new burger and eatery

--- controllers/burgers_controller.rb
+++ controllers/burgers_controller.rb
@@ -6,3 +6,14 @@ get '/burgers' do
   @burgers = Burger.all()
   erb ( :"burgers/index" )
 end
+
+post '/burgers' do
+  @burger = Burger.new(params)
+  @burger.save()
+  redirect to '/burgers'
+end
+
+get '/burgers/new' do
+  erb(:"burgers/new")
+end
+
--- controllers/eateries_controller.rb
+++ controllers/eateries_controller.rb
@@ -6,3 +6,13 @@ get '/eateries' do
   @eateries = Eatery.all()
   erb ( :"eateries/index" )
 end
+
+get '/eateries/new' do
+  erb(:"eateries/new")
+end
+
+post '/eateries' do
+  @eatery = Eatery.new(params)
+  @eatery.save()
+  redirect to '/eateries'
+end
